0f28428f22a4e5e17192db30f52acf5706b6162c
ariel32lubin <96148090+ariel32lubin@users.noreply.github.com>
2023-11-21 09:49:17 -0600

    Create playwright.py

---
+++ playwright.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from typing import List, Union
+from playwright.sync_api import Playwright, Page
+from bs4 import BeautifulSoup
+import logging
+import time
+import datetime
+
+def get_report_data_from_backstop(playwright: Playwright, report_page_url: str, backstop_username: str, backstop_password: str) -> pd.DataFrame:
+    """
+    Scrapes data from a report page on the Backstop Solutions platform, including sign-in, loading the report, and parsing a data table. Report limit 500 records.
+
+    Args:
+        playwright (Playwright): Playwright instance.
+        report_page_url (str): The URL of the report page to scrape.
+        backstop_username (str): The username for logging into Backstop Solutions.
+        backstop_password (str): The password for logging into Backstop Solutions.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the scraped data.
+
+    Raises:
+        Exception: If there's an issue during the scraping process.
+
+    Example:
+        get_report_data_from_backstop(playwright_instance, 'https://dsa.backstopsolutions.com/backstop/your_report_url', 'your_username', 'your_password')
+    """
+    browser = playwright.chromium.launch(headless=False, channel='msedge')
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
+
+    try:
+        # Sign into Backstop
+        logger.info('signing in...')
+        page.goto("https://dsa.backstopsolutions.com/backstop/")
+        page.get_by_placeholder("Please enter your username").click()
+        page.get_by_placeholder("Please enter your username").fill(backstop_username)
+        page.get_by_placeholder("Please enter your password").click()
+        page.get_by_placeholder("Please enter your password").fill(backstop_password)
+        page.get_by_role("button", name="Log In").click()
+
+        # Load the report
+        logger.info('loading report')
+        page.goto(report_page_url, timeout=2_000_000)
+
+        # Parse the data table
+        logger.info('parsing data table')
+        soup = BeautifulSoup(page.content(), "html.parser")
+        table = soup.find('table')  # Make sure to specify the HTML structure of your table here
+        report_data_df = get_df_from_html_table(table)  # You need to implement get_df_from_html_table
+
+    except:
+        logger.info('Saving trace...')
+        context.tracing.stop(path=f"traces/get_report_data_from_backstop.zip")
+
+    finally:        
+        context.close()
+        browser.close()
+
+    # Save the DataFrame to a CSV file
+    report_data_df.to_csv(r'data/report_data.csv', index=False)
+
+    logger.info('Done.')
+
+    return report_data_df
