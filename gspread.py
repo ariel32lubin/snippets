875253231bb4ba3eb56cb168bea529ddc63370c9
ariel32lubin <96148090+ariel32lubin@users.noreply.github.com>
2023-11-27 11:10:48 -0600

    Update gspread.py

--- gspread.py
+++ gspread.py
@@ -1,4 +1,3 @@
-CREDENTIALS_FILE = r'credentials/google_credentials.json'
-gc = gspread.service_account(filename=CREDENTIALS_FILE)
-spreadsheet = gc.open_by_key('1L8bLAtXd4Fjym2tGqsDLvxtDmSRe-8NuIUyg5WMCJqU')
-print(spreadsheet.worksheets())
+gc = gspread.service_account(filename="credentials.json")
+sh = gc.open_by_key('<your_key>')
+ws = sh.sheet1
