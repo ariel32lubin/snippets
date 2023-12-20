912aa110e60cdfc9ec95eb898295f3962ea59fed
ariel32lubin <96148090+ariel32lubin@users.noreply.github.com>
2023-12-20 12:56:16 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -34,3 +34,35 @@ def log_function(func: Callable[..., Any]) -> Callable[..., Any]:
         return result
 
     return wrapper
+
+def send_error_email(script_name: str, error_message: str) -> None:
+    """
+    Send an error email using Microsoft Outlook.
+
+    Args:
+        script_name (str): The name of the script where the error occurred.
+        error_message (str): The error message to include in the email.
+
+    Returns:
+        None
+
+    Example:
+        script_name = "YourScriptName.py"
+        error_message = "An error occurred in the script."
+        send_error_email(script_name, error_message)
+    """
+    import win32com.client as win32
+
+    # Create an Outlook application instance
+    outlook = win32.Dispatch('Outlook.Application')
+
+    # Create a new email
+    mail = outlook.CreateItem(0)
+
+    # Set email properties
+    mail.To = 'alubin@fiducient.com'
+    mail.Subject = f'Error in {script_name}'
+    mail.Body = f'There was an error in {script_name}:\n\n{error_message}'
+
+    # Send the email
+    mail.Send()
