64c71bc41464cf1b45cdf46b3aba072d52a08a90
ariel32lubin <96148090+ariel32lubin@users.noreply.github.com>
2024-02-21 11:49:43 -0600

    Update nbconvert.py

--- nbconvert.py
+++ nbconvert.py
@@ -1,10 +1,14 @@
-import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-
-# function to run notebook as py
 def run_notebook(notebook_path):
+    # log starting 
+    print(f"Running notebook {notebook_path}")
     with open(notebook_path) as ff:
         nb_in = nbformat.read(ff, nbformat.NO_CONVERT)
         
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-    nb_out = ep.preprocess(nb_in)
+    nb_out, resources = ep.preprocess(nb_in)  # Unpack the tuple to get the notebook object and resources
+
+    # Save output notebook
+    with open(notebook_path, mode="wt") as ff:
+        nbformat.write(nb_out, ff)
+    # log completion
+    print(f"Notebook {notebook_path} ran successfully")
