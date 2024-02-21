7aaab4bb89e8d7bbeb1eef62ae5a8d046a58836a
ariel32lubin <96148090+ariel32lubin@users.noreply.github.com>
2024-02-21 11:50:29 -0600

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,21 +1,21 @@
 import threading
 import time
 import schedule
+from utils import run_notebook  # Assuming run_notebook is defined in utils
 
-def job():
-    print("I'm running on thread %s" % threading.current_thread())
-
-def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
+def run_threaded(job_func, *args, **kwargs):
+    job_thread = threading.Thread(target=job_func, args=args, kwargs=kwargs)
     job_thread.start()
 
-schedule.every(10).seconds.do(run_threaded, job)
-schedule.every(10).seconds.do(run_threaded, job)
-schedule.every(10).seconds.do(run_threaded, job)
-schedule.every(10).seconds.do(run_threaded, job)
-schedule.every(10).seconds.do(run_threaded, job)
+# Schedule get auths notebook every day at 8 pm 
+schedule.every().day.at("20:00").do(run_threaded, run_notebook, notebook_path="scripts/00_get_auths.ipynb")
 
+# Schedule push daily update notebook every day at 8:30 pm
+schedule.every().day.at("20:30").do(run_threaded, run_notebook, notebook_path="scripts/01_push_daily_update.ipynb")
 
-while 1:
+# Run get auths notebook right now in a separate thread
+run_threaded(run_notebook, notebook_path="01_push_daily_update.ipynb")
+
+while True:
     schedule.run_pending()
     time.sleep(1)
